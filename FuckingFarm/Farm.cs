63b487b44d2d4b4e750ee143a08e88e3b2d0a62c
saint12maloj <saint12maloj@gmail.com>
2018-11-23 22:54:06 +0300

    2 метода без Product

--- FuckingFarm/Farm.cs
+++ FuckingFarm/Farm.cs
@@ -10,24 +10,47 @@ namespace FuckingFarm
     {
         public string FarmName { get; set; }
         public double FarmSquare { get; set; }
-        public List <Gardenbed> Gardenbeds { get; set; }
-        public List <Building> Buildings { get; set; }
+        public List<Gardenbed> Gardenbeds { get; set; }
+        public List<Building> Buildings { get; set; }
 
-        public void AboutNameAndSquare ()
+        public void AboutNameAndSquare()
         {
             Console.WriteLine($"The name of the farm is {FarmName}. \nThe total square of the farm is {FarmSquare} sq.m..");
         }
 
-        public void AboutGardenbeds ()
+        public void AboutGardenbeds()
         {
-            Console.WriteLine("The list of the gardenbeds is:");
+            Console.WriteLine("The Gardenbeds");
             for (int i = 0; i < Gardenbeds.Count; i++)
-            for (int j = 0; j < Gardenbeds[i].Plants.Count; j++)
-            {
-                Console.WriteLine($"A {Gardenbeds[i].Plants[j].PlantName} gardenbed;");
-            }
+                for (int j = 0; j < Gardenbeds[i].Plants.Count; j++)
+                {
+                    Console.WriteLine("The list of the plants at the gardenbeds includes:");
+                    Console.WriteLine($"A {Gardenbeds[i].Plants[j].PlantName} gardenbed;");
+                    Console.WriteLine($"It's season of planting is {Gardenbeds[i].Plants[j].PlantSeasonOfPlanting};");
+                    Console.WriteLine($"It's season of harvesting is {Gardenbeds[i].Plants[j].PlantSeasonOfHarvesting};");                    
+                    Console.WriteLine($"It's square at the gardenbed is {Gardenbeds[i].Plants[j].PlantSquareAtTheGardenbed} sq.m.;");
+                }
+
+        }
+        public void AboutBuildings()
+        {
+            
+            for (int i = 0; i < Buildings.Count; i++)
+                for (int j = 0; j < Buildings[i].Animals.Count; j++)
+                {                   
+                    
+                    Console.WriteLine($"The list of the animals at the farm includes {Buildings[i].Animals[j].AnimalName};");
+                    Console.WriteLine($"The product they produce is {Buildings[i].Animals[j].Products};");
+                    Console.WriteLine($"They live in a {Buildings[i].BuildingName};");
+                    
+                    Console.WriteLine($"There are {Buildings[i].BuildingAnimalsQuantity} of them in a building;");
+                    Console.WriteLine($"The square of each building is {Buildings[i].BuildingSquare} sq.m.;");                    
+
+                }
+
             Console.ReadLine();
         }
+
 
     }
 
--- FuckingFarm/Program.cs
+++ FuckingFarm/Program.cs
@@ -8,25 +8,25 @@ namespace FuckingFarm
 {
    public class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
             Product milk = new Product() { ProductName = "milk", ProductWeight = 10};
             Product anEgg = new Product() { ProductName = "An egg", ProductWeight = 20 };
             Product meat = new Product() { ProductName = "meat", ProductWeight = 30 };
             
-            Animal aCow = new Animal () {AnimalName = "A cow", Products = milk };
-            Animal aHen = new Animal() { AnimalName = "A hen", Products = anEgg};
-            Animal aPig = new Animal() { AnimalName = "A pig", Products = meat };
+            Animal aCow = new Animal () {AnimalName = "cows", Products = milk };
+            Animal aHen = new Animal() { AnimalName = "hens", Products = anEgg};
+            Animal aPig = new Animal() { AnimalName = "pigs", Products = meat };
 
-            Building aCowshed = new Building() { BuildingName = "A cowshed", BuildingSquare = 30, BuildingAnimalsQuantity = 20, Animals = new List <Animal>() };
+            Building aCowshed = new Building() { BuildingName = "cowshed", BuildingSquare = 30, BuildingAnimalsQuantity = 20, Animals = new List <Animal>() };
             aCowshed.Animals.Add(aCow);
             aCowshed.Animals.Add(aPig);
-            Building aHenhous = new Building() { BuildingName = "A pigsty", BuildingSquare = 20, BuildingAnimalsQuantity = 10, Animals = new List <Animal> ()}; 
+            Building aHenhous = new Building() { BuildingName = "pigsty", BuildingSquare = 20, BuildingAnimalsQuantity = 10, Animals = new List <Animal> ()}; 
             aHenhous.Animals.Add(aHen);
 
-            Plant potato = new Plant() { PlantName = "potato", PlantSeasonOfPlanting = "Spring", PlantSeasonOfHarvesting = "Autumn", PlantSquareAtTheGardenbed = 30};
-            Plant cucubbers = new Plant() { PlantName = "cucumber", PlantSeasonOfPlanting = "Spring", PlantSeasonOfHarvesting = "Summer", PlantSquareAtTheGardenbed = 20};
-            Plant garlic = new Plant() { PlantName = "garlic", PlantSeasonOfPlanting = "Winter", PlantSeasonOfHarvesting = "Summer", PlantSquareAtTheGardenbed = 10};
+            Plant potato = new Plant() { PlantName = "potato", PlantSeasonOfPlanting = "spring", PlantSeasonOfHarvesting = "autumn", PlantSquareAtTheGardenbed = 30};
+            Plant cucubbers = new Plant() { PlantName = "cucumber", PlantSeasonOfPlanting = "spring", PlantSeasonOfHarvesting = "summer", PlantSquareAtTheGardenbed = 20};
+            Plant garlic = new Plant() { PlantName = "garlic", PlantSeasonOfPlanting = "winter", PlantSeasonOfHarvesting = "summer", PlantSquareAtTheGardenbed = 10};
 
             Gardenbed gardenbedPotato = new Gardenbed() {GardenbedSquare = 30, Plants = new List <Plant> () };
             Gardenbed gardenbedCucumbers = new Gardenbed() {GardenbedSquare = 20, Plants = new List <Plant> () };
@@ -44,6 +44,7 @@ namespace FuckingFarm
 
             myFarm.AboutNameAndSquare();
             myFarm.AboutGardenbeds();
+            myFarm.AboutBuildings();
 
             Console.ReadLine();
 
